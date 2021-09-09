d506bd3952c118da62cd7831a1280cc5c5aa8536
jamie <rmk1075@gmail.com>
2021-09-09 14:02:29 +0000

    test and fix some errors

--- src/main.py
+++ src/main.py
@@ -10,116 +10,6 @@ import argparse
 from pandas import json_normalize
 from typing import List
 
-# api 요청에서 에러발생 시 에러 로그 생성하고 시스템 종료하는 함수
-def error_log(response: requests.models.Response, api_name: str):
-    # TODO: error_log_path
-    # error_log_path = parent_path + '/log/error_log.log'
-    error_log_path = '../log/error_log/log'
-    if not response.json()['ok']:
-        with open(error_log_path, 'a') as error_log_file:
-            error_log_message = f'[error][{api_name}][{datetime.datetime.now()}] call \'{api_name}\' api error. \n[error message]{response.json()["error"]}\n'
-            error_log_file.write(error_log_message)
-
-            print(error_log_message)
-            return True
-    return False
-
-# til 작성한 사용자들을 list로 반환하는 함수
-def find_user_list(slack_token: str, channel_id: str):
-    user_list = set([])
-
-    # 대화 히스토리 api
-    # https://api.slack.com/methods/conversations.history
-    URL = 'https://slack.com/api/conversations.history'
-
-    # oldest 시간 구하기 (현재 시간에서 1day를 빼고 계산)
-    oldest = time.mktime((datetime.datetime.now() - datetime.timedelta(days = 1)).timetuple())
-
-    # 파라미터
-    params = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'token': slack_token,
-        'channel': channel_id,
-        'oldest': oldest
-    }
-
-    response = requests.get(URL, params=params)
-
-    if error_log(response, 'conversations.history'):
-        print('api error')
-        sys.exit(1)
-    
-    # 어제 날짜 확인을 위한 patter 생성
-    yesterday = datetime.date.today() - datetime.timedelta(1)
-    pattern = re.compile(yesterday.strftime('\\[%Y.%m.%d\\]') + '*')
-
-    # 메세지 확인
-    # 1. slack bot 제외 ('U01NKV5PPCP')
-    # 2. 날짜 확인
-    for message in response.json()['messages']:
-        if message['user'] == 'U01NKV5PPCP':
-            continue
-
-        if pattern.match(message['text']):
-            user_list.add(message['user'])
-
-    return list(user_list)
-
-# user_id에 해당하는 사용자의 이름을 str로 반환하는 함수
-def find_user_name(slack_token: str, channel_id: str, user_id: str):
-    # 사용자 정보 조회 api
-    # https://api.slack.com/methods/users.info
-    URL = 'https://slack.com/api/users.info'
-
-    # 파라미터
-    params = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'token': slack_token,
-        'user': user_id,
-    }
-
-    response = requests.get(URL, params=params)
-
-    if error_log(response, 'user.info'):
-        print('api error')
-        sys.exit(1)
-    
-    return response.json()['user']['real_name']
-
-# til 작성한 사용자들의 이름을 str, 숫자를 int로 반환하는 함수
-def find_user_names(slack_token: str, channel_id: str, user_list: List[str]):
-    user_names, user_cnt = '', 0
-    for user_id in user_list:
-        user_names += find_user_name(slack_token, channel_id, user_id) + ', '
-        user_cnt += 1
-    
-    return user_names, user_cnt
-
-# slack 채널에 메시지 작성하는 함수
-def post_message(slack_token, channel_id, message):
-    # 메시지 전송 api method (chat.postMessage)
-    # https://api.slack.com/methods/chat.postMessage
-    URL = 'https://slack.com/api/chat.postMessage'
-
-    # 파라미터
-    data = {
-        'Context_Type': 'application/x-www-form-urlencoded',
-        'token': slack_token,
-        'channel': channel_id,
-        'text': message
-    }
-
-    # API 호출
-    response = requests.post(URL, data=data)
-
-    # response error check
-    if error_log(response, 'chat.postMessage'):
-        print('api error')
-        sys.exit(1)
-
-    print(message)
-
-
 class Slack:
     def __init__(self, **kwargs):
         self._token_path = kwargs["token_path"]
@@ -293,9 +183,9 @@ class ApiHandler:
         response = self.__api.api_request(method=method, params=params)
         if not response:
             sys.stderr.write(f"Failed to {method}.\n")
-            return False
+            return None
         else:
-            return True
+            return message
 
 
 class ApiException(Exception):
@@ -319,7 +209,7 @@ parser.add_argument("-t", "--token_name", help="name of token", type=str, defaul
 parser.add_argument("-c", "--channel_name", help="name of slack channel", type=str, default='til')
 # parser.add_argument("-c", "--channel_name", help="name of slack channel", type=str, default='today-i-learned')
 parser.add_argument("-u", "--url", help="api server url", type=str, default='https://slack.com/api/')
-parser.add_argument("--alert", help="excute alert mode", action="store_true")
+parser.add_argument("-a", "--alert", help="excute alert mode", action="store_true")
 
 if __name__ == "__main__":
     # 현재 시간 log
@@ -333,60 +223,53 @@ if __name__ == "__main__":
 
     # init Slack
     slack = Slack(token_path=token_path, token_name=token_name, channel_name=channel_name)
-    if not slack:
+    if slack == None:
         sys.stderr.write("Failed to init Slack instance.\n")
         sys.exit(-1)
 
     # init Api
     api = Api(url=url)
-    if not api:
+    if api == None:
         sys.stderr.write("Failed to init Api instance.\n")
         sys.exit(-1)
 
     # create ApiHandler
     api_handler = ApiHandler(api)
-    if not api_handler:
-        sys.stderr.wrtie("Failed to create ApiHandler instance.\n")
+    if api_handler == None:
+        sys.stderr.write("Failed to create ApiHandler instance.\n")
         sys.exit(-1)
 
     # conversations.list api를 사용하여서 slack 대화 채널 id 조회
     # channel_id = find_channel_id(slack.token, slack.channel_name)
     channel_id = api_handler.get_channel_id(slack.token, slack.channel_name)
-    if not channel_id:
+    if channel_id == None:
         sys.stderr.write("Failed to get channel_id\n")
         sys.exit(-1)
 
     # bot이 전송할 메세지 생성
     # argument '--alert' 사용시 alert 모드로 실행
     if args.alert:
+        print("alert mode")
         message = f'현재시간 {datetime.datetime.now().strftime("%H:%M")}입니다.\n아직 til을 작성하지 않으신 분은 빠르게 작성해주세요.'
     else:
-        # TODO: test
+        print("counting mode")
+
         # conversations.history api를 사용하여서 til 작성한 user들의 id 조회
-        # user_list = find_user_list(slack_token, channel_id)
-        # user_list = find_user_list(slack.token, channel_id)
         user_list = api_handler.get_user_list(slack.token, channel_id)
-        if not user_list:
-            sys.stderr.write("Failed to get user_list")
+        if user_list == None:
+            sys.stderr.write("Failed to get user_list\n")
             sys.exit(-1)
 
-        print(user_list)
-
         # user_list의 user id를 통해서 user 이름 조회
-        # user_names, user_cnt = find_user_names(slack_token, channel_id, user_list)
-        # user_names, user_cnt = find_user_names(slack.token, channel_id, user_list)
         user_names = api_handler.get_user_names(slack.token, channel_id, user_list)
-        if not user_names:
+        if user_names == None:
             sys.stderr.write("Failed to get user_names\n")
             sys.exit(-1)
 
         user_cnt = len(user_names)
         message = f'금일 til을 작성한 인원은 {user_names}총 {user_cnt}명 입니다.\n오늘 하루도 수고하셨습니다.'
 
-
     # 메세지 전송
-    # post_message(slack_token, channel_id, message)
-    # post_message(slack.token, channel_id, message)
-    if not api_handler.post_message(slack.token, channel_id, message):
+    if api_handler.post_message(slack.token, channel_id, message) == None:
         sys.stderr.write("Failed to post message")
         sys.exit(-1)
