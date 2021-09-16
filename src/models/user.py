f794c877bcce69f95b0915c7b1ac88f1326f4f0b
jamie <rmk1075@gmail.com>
2021-09-16 20:58:23 +0900

    add User, Message models

--- src/models/user.py
+++ src/models/user.py
@@ -4,7 +4,7 @@ class User:
         self.__name = kwargs['name']
         self.__deleted = kwargs['deleted']
         self.__real_name = kwargs['real_name']
-        self.__email = kwargs['email']
+        # self.__email = kwargs['email']
 
         # self.__count
         # self.__crtnDate
